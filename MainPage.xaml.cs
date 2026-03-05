8ae3719a9a03e0cab333d433d48ab644ec8fe61c
Richard Trygg Svartling <tryggrichard@gmail.com>
2026-03-05 12:35:26 +0100

    La till en popup (använder maui community toolkit) för framtida bokning i en folder som heter popups när man trycket på vald tid

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using PlannerApp.Callender;
+using PlannerApp.popups;
+using CommunityToolkit.Maui.Views;
 
 namespace PlannerApp
 {
@@ -32,35 +34,14 @@ namespace PlannerApp
             CalenderContainer.Content = _calender.Calendar;
             
             // Prenumererar på HourSelected-eventet för att hantera när användaren väljer en timme
-            // Detta event triggas när användaren klickar på en specifik timme i kalendern
-            _calender.HourSelected += (sender, dateTime) =>
+            _calender.HourSelected += async (sender, dateTime) =>
             {
-                // Visar en bekräftelsedialog med valt datum och tid
-                // dateTime innehåller både datum och timme användaren valde
-                // Format: yyyy-MM-dd HH:mm (t.ex. 2024-01-15 14:00)
-                DisplayAlert("Timme vald", $"Du valde: {dateTime:yyyy-MM-dd HH:mm}", "OK");
+                var popup = new BookingPopUp(dateTime);
+                var result = await this.ShowPopupAsync(popup);
                 
-                // TODO: Här kan du lägga till funktionalitet för att:
-                // - Spara en bokning/aktivitet för vald tid
-                // - Navigera till en ny sida för att skapa en händelse
-                // - Visa befintliga händelser för vald timme
-                // - Uppdatera en lista med valda tider
             };
-            
-            // Du kan också prenumerera på DateSelected om du vill hantera dagval separat:
-            // _calender.DateSelected += (sender, dateTime) =>
-            // {
-            //     // Kod som körs när användaren väljer en ny dag i kalendern
-            // };
         }
         
         #endregion
-        
-        // Tips för vidareutveckling:
-        // 1. Lägg till databas för att spara bokningar/aktiviteter
-        // 2. Skapa en ny sida för att visa detaljer när en timme väljs
-        // 3. Lägg till visuella markeringar för bokade timmar
-        // 4. Implementera notifikationer för kommande händelser
-        // 5. Lägg till möjlighet att välja flera timmar för en bokning
     }
 }
