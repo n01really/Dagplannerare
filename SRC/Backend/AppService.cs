da058303ee430fb1780c423cf50cc400f8013f25
Richard Trygg Svartling <tryggrichard@gmail.com>
2026-04-09 09:26:02 +0200

    fixade en lista för alla instalerade appr genom att använda Win32. jag samlar in appars namn version och Publisher

---
+++ SRC/Backend/AppService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using PlannerApp.SRC.Models;
+
+
+namespace PlannerApp.SRC.Backend
+{
+    public class AppService
+    {
+        public List<AppsModels> GetInstalledApps()
+        {
+            var apps = new List<AppsModels>();
+            string[] registryKeys = {
+            @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+            @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        };
+            foreach (string Key in registryKeys)
+            {
+                using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                using (var subKey = hklm.OpenSubKey(Key))
+                {
+                    if (subKey != null)
+                    {
+                        foreach (var subKeyName in subKey.GetSubKeyNames())
+                        {
+                            using (var appKey = subKey.OpenSubKey(subKeyName))
+                            {
+                                var name = appKey.GetValue("DisplayName") as string;
+                                if (!string.IsNullOrEmpty(name))
+                                {
+                                    apps.Add(new AppsModels
+                                    {
+                                        Name = name,
+                                        Version = appKey.GetValue("DisplayVersion") as string,
+                                        Publisher = appKey.GetValue("Publisher") as string
+
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            return apps;
+        }
+    }
+}
+
+
+
+
---
+++ SRC/Models/AppsModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.SRC.Models
+{
+    public class AppsModels
+    {
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Version { get; set; }
+       
+    }
+}
