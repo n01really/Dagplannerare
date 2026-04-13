3c296f1e8538531c8ff45d9dac2d9ea94c226228
Richard Trygg Svartling <tryggrichard@gmail.com>
2026-04-13 09:08:24 +0200

    uppdaterad schedualmodel för att ta in title och bakgrunds färg

--- SRC/Models/SchedualModel.cs
+++ SRC/Models/SchedualModel.cs
@@ -12,11 +12,13 @@ namespace PlannerApp.SRC.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        public string title { get; set; }
         public string AppName { get; set; }
         public int AppId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string Description { get; set; }
         public DateTime DateTime { get; set; }
+        public string BackgroundColor { get; set; }
     }
 }
